2642313282c67dfbae5d9608682fa8a327fa516f
duck69exe <107806061+duck69exe@users.noreply.github.com>
2022-06-30 23:39:54 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,17 +8,17 @@ import json
 import os
 import requests
 from selenium.webdriver.common.by import By
-options = uc.ChromeOptions()
-
-low_word = "abcdefghijklkmnopqrstuvwxyz"
-upper_word = "ABDCEFGHIJKLMNOPQRSTUVWXYZ"
-number = "1234567890"
-symbols = "!@#$%&*"
-username_for = low_word
-password_for = low_word + upper_word + number + symbols
-long_password = 16
-long_username = 12
 while True:
+  options = uc.ChromeOptions()
+
+  low_word = "abcdefghijklkmnopqrstuvwxyz"
+  upper_word = "ABDCEFGHIJKLMNOPQRSTUVWXYZ"
+  number = "1234567890"
+  symbols = "!@#$%&*"
+  username_for = low_word
+  password_for = low_word + upper_word + number + symbols
+  long_password = 16
+  long_username = 12
   options.add_argument('--no-first-run --no-service-autorun --password-store=basic') #wlacz to jak juz nie bedzie dev test
   #options.user_data_dir = "rawr"
   options.add_argument("--window-size=1920,1080")
